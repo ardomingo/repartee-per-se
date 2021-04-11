0b8c91d40742725b50357c2c023e626e67a18c9d
ardomingo <andres.r.domingo@gmail.com>
2021-04-11 11:34:41 -0700

    Rebuild

--- eda/brady_stats.R
+++ eda/brady_stats.R
@@ -66,7 +66,7 @@ avgs_table_set <- rbind(league1, brady_early, brady_late)
 datatable(avgs_table_set,           
       rownames = FALSE,
       caption = htmltools::tags$caption(style = 
-            'caption-side: top; text-align: center; color:black; font-size:200% ;','Brady vs. The League'),
+            'caption-side: top; text-align: center; color:blue; font-size:200% ;','Brady vs. The League'),
       options = list(
             order = list(list(2, 'dsc')),
             columnDefs = list(list(className = 'dt-center', targets = 0:6)))) %>% 
@@ -76,6 +76,8 @@ datatable(avgs_table_set,
 #Get Player data and categorize by year
 passing_2015 <- read.csv("X:/OneDrive/02. Data/NFL Data/NFl DataPassing2015.csv")
 passing_2015$Year <- 2015
+
+
 
 passing_2016 <- read.csv("X:/OneDrive/02. Data/NFL Data/NFl DataPassing2016.csv")
 passing_2016$Year <- 2016
@@ -177,3 +179,164 @@ datatable(percentiles,
       target = 'row',
       backgroundColor = styleEqual("2020", 'skyblue')) %>% 
   formatPercentage(columns = 5)
+
+
+
+
+###########################################
+
+library(formattable)
+formattable(percentiles, align = c("l", rep("r", NCOL(percentiles) - 1)))
+formattable(avgs_table_set, align = c("l", rep("r", NCOL(avgs_table_set) - 1)))
+
+
+
+
+league_chart <- 
+  pivot_longer(percentiles, Att:IntPCT, names_to = "Stat", values_to = "Rank")
+
+league_chart$Rank <- as.double(league_chart$Rank)
+
+
+
+
+
+
+
+league_chart_PCTs <- percentiles[,c(1:2, 9:14)]
+#league_chart_trim1 <- league_chart_trim[ , c(1, 3:8)]
+
+ 
+league_chart_PCTs %>% 
+group_by(Player, Year) %>% 
+  summarise(Year, AttPCT, CmpPCT, CmplPCT, YdsPCT, TDPCT, IntPCT) 
+  
+
+brady_PCTs <- filter(league_chart_PCTs, grepl("Brady|BRADY", Player))
+brady_PCTs <- brady_PCTs[order(brady_PCTs$Player, desc(brady_PCTs$Year)), ]
+
+
+formattable(brady_PCTs, list(
+  AttPCT = color_bar("pink"),
+  CmpPCT = color_bar("silver"),
+  CmplPCT = color_bar("lightblue"),
+  YdsPCT = color_bar("pink"),
+  TDPCT = color_bar("silver"),
+  IntPCT = color_bar("lightblue")))
+
+
+
+#Build Table  
+datatable(percentiles[ ,c(1:2, 9, 11:14)],
+          rownames = FALSE,
+          caption = htmltools::tags$caption(style = 
+                                              'caption-side: top; text-align: center; color: goldenrod; font-size:150% ;',
+                                            'Brady vs. The Players'),
+          options = list(pageLength = 15,
+                         order = list(list(2, 'dsc')),
+                         columnDefs = list(list(className = 'dt-center', targets = 2:6)),
+          initComplete = JS("
+                         function(settings, json) {
+                         $(this.api().table().header()).css({
+                         'background-color': 'slateblue',
+                         'color': '#fff'
+                         });
+                         }"))) %>% 
+  formatStyle(
+    'Player',
+    target = 'row',
+    backgroundColor = styleEqual("T. BRADY", 'aqua')) %>% 
+  formatStyle(
+    'Year',
+    target = 'row',
+    backgroundColor = styleEqual("2020", 'skyblue')) 
+  
+
+
+               
+  datatable(avgs_table_set,           
+            rownames = FALSE,
+            caption = htmltools::tags$caption(style = 
+                                                'caption-side: top; text-align: center; color: goldenrod; font-size:150% ;','Brady vs. The League'),
+            options = list(
+              order = list(list(2, 'dsc')),
+              columnDefs = list(list(className = 'dt-center', targets = 0:6)),
+              initComplete = JS("
+                         function(settings, json) {
+                         $(this.api().table().header()).css({
+                         'background-color': 'slateblue',
+                         'color': '#fff'
+                         });
+                         }"))) %>%
+    formatRound(columns=c(1:5), digits=2)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  datatable(avgs_table_set,           
+            rownames = FALSE,
+            options = list(
+              dom = 't',
+              order = list(list(2, 'dsc')),
+              columnDefs = list(list(className = 'dt-center', targets = 0:6)),
+              initComplete = JS("
+                         function(settings, json) {
+                         $(this.api().table().header()).css({
+                         'background-color': 'silver',
+                         'color': 'blue'
+                         });
+                         }"))) %>%
+    formatRound(columns=c(1:5), digits=2)
+  
+  
+  
+  
+  formattable(brady_PCTs,
+              list(
+                AttPCT = color_bar("pink"),
+                CmpPCT = color_bar("silver"),
+                CmplPCT = color_bar("lightblue"),
+                YdsPCT = color_bar("pink"),
+                TDPCT = color_bar("silver"),
+                IntPCT = color_bar("lightblue")))
+
+  
+  
+  
+  
+  formattable(avgs_table_set,           
+            rownames = FALSE,
+            options = list(
+              dom = 't',
+              order = list(list(2, 'dsc')),
+              columnDefs = list(list(className = 'dt-center', targets = 0:6)),
+              initComplete = JS("
+                         function(settings, json) {
+                         $(this.api().table().header()).css({
+                         'background-color': 'silver',
+                         'color': 'blue'
+                         });
+                         }"))) %>%
+    formatRound(columns=c(1:5), digits=2)
+
+  
+  
+  formattable(avgs_table_set,
+              list(
+                Att = color_tile("transparent", "pink"),
+                Cmp = color_tile("transparent", "lightgray"),
+                Yds = color_tile("transparent", "lightblue"),
+                TD = color_tile("transparent", "pink"),
+                Int = color_tile("lightgray", "transparent"))) 
+  
+  
+  
+  avgs_table_set1 <- avgs_table_set[order(avgs_table_set$Party, desc(avgs_table_set$Year)), ]
+  
